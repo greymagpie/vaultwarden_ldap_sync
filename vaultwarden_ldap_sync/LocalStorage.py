e4116ef4d2901e0a273548a3ab796a63790e9ceb
Alexander Krischan <akrischan@vmlab.at>
2024-04-11 20:43:37 +0200

    Rewrite localstore class for mariadb support

--- vaultwarden_ldap_sync/LocalStorage.py
+++ vaultwarden_ldap_sync/LocalStorage.py
@@ -1,7 +1,6 @@
-import os.path
-import sqlite3
 import time
 import logging
+import mysql.connector
 from typing import Tuple, List
 
 ALLOWED_USER_STATES = ['ENABLED', 'DISABLED', 'DELETED']
@@ -9,92 +8,49 @@ ALLOWED_USER_STATES = ['ENABLED', 'DISABLED', 'DELETED']
 
 class LocalStore:
 
-    def __init__(self, sqlite_file: str):
-        self.con = sqlite3.connect(sqlite_file)
-        self.init_db()
+    def __init__(self, user: str, password: str, host: str, database: str):
+        self.con = mysql.connector.connect(user=user, password=password, host=host, database=database)
 
-    def init_db(self):
-        schema = '''
-                create table if not exists Users
-        (
-            id           integer not null
-                constraint Users_pk
-                    primary key autoincrement,
-            invite_email TEXT    not null,
-            vw_email     TEXT    not null,
-            vw_user_id   TEXT    not null,
-            last_touched TEXT    not null,
-            state        TEXT    not null
-        );
-        '''
-
-        self.con.cursor().execute(schema)
-        self.con.commit()
-
-    def get_all_users(self) -> tuple:
-        """
-        Get all managed users (-> All users which have been invited by this script)
-        :return: A 3-tuple containing enabled, disabled/deleted and all users
-        """
-        res = self.con.cursor().execute("SELECT invite_email, vw_email, vw_user_id, state FROM Users;")
+    def get_all_users(self) -> Tuple[dict, dict, dict]:
+        cursor = self.con.cursor()
+        cursor.execute("SELECT invite_email, vw_email, vw_user_id, state FROM Users;")
         enabled = {}
         disabled = {}
-        for invite_email, vw_email, vw_user_id, state in res.fetchall():
+        for invite_email, vw_email, vw_user_id, state in cursor.fetchall():
             if state == 'ENABLED':
-                enabled[vw_user_id] = {
-                    'vw_email': vw_email,
-                    'invite_email': invite_email
-                }
+                enabled[vw_user_id] = {'vw_email': vw_email, 'invite_email': invite_email}
             else:
-                disabled[vw_user_id] = {
-                    'vw_email': vw_email,
-                    'invite_email': invite_email
-                }
+                disabled[vw_user_id] = {'vw_email': vw_email, 'invite_email': invite_email}
         return enabled, disabled, {**enabled, **disabled}
 
     def register_user(self, user_email: str, user_id: str):
-        """
-        Register user as a 'managed user'
-        :param user_email: Invitation Email
-        :param user_id: User ID returned by vaultwarden
-        :return: None
-        """
         cursor = self.con.cursor()
         try:
             cursor.execute(
-                'INSERT INTO Users (invite_email, vw_email, vw_user_id, last_touched, state) VALUES (?,?,?,?,?)',
-                (user_email, user_email, user_id, time.time(), 'ENABLED'))
+                'INSERT INTO Users (invite_email, vw_email, vw_user_id, last_touched, state) VALUES (%s,%s,%s,%s,%s)',
+                (user_email, user_email, user_id, time.strftime('%Y-%m-%d %H:%M:%S'), 'ENABLED'))
             self.con.commit()
-        except sqlite3.IntegrityError as e:
-            logging.warning('Could not insert user {}: {}'.format(user_email, e))
+        except mysql.connector.IntegrityError as e:
+            logging.warning(f'Could not insert user {user_email}: {e}')
 
-    def set_user_state(self, vw_user_id: str, user_state):
-        """
-        Set user state (meant to reflect the state in vaultwarden)
-        :param vw_user_id: Vaultwarden User ID
-        :param user_state: One of ENABLED,DISABLED or DELETED
-        :return: None
-        """
+    def set_user_state(self, vw_user_id: str, user_state: str):
         if user_state not in ALLOWED_USER_STATES:
-            raise ValueError('Invalid user state. Must be one of: {}'.format(ALLOWED_USER_STATES))
+            raise ValueError(f'Invalid user state. Must be one of: {ALLOWED_USER_STATES}')
         else:
-            self.con.cursor().execute('UPDATE Users SET state = ?, last_touched = ? WHERE vw_user_id = ?',
-                                      (user_state, time.time(), vw_user_id))
+            cursor = self.con.cursor()
+            cursor.execute('UPDATE Users SET state = %s, last_touched = %s WHERE vw_user_id = %s',
+                           (user_state, time.strftime('%Y-%m-%d %H:%M:%S'), vw_user_id))
             self.con.commit()
 
     def update_vw_email(self, vw_user_id: str, new_vw_email: str):
-        """
-        Update vaultwarden email (after user updated his email in vaultwarden)
-        :param vw_user_id: Vaultwarden User ID
-        :param new_vw_email: New email
-        :return: None
-        """
-        self.con.cursor().execute('UPDATE Users SET vw_email = ?, last_touched = ? WHERE vw_user_id = ?',
-                                  (new_vw_email, time.time(), vw_user_id))
+        cursor = self.con.cursor()
+        cursor.execute('UPDATE Users SET vw_email = %s, last_touched = %s WHERE vw_user_id = %s',
+                       (new_vw_email, time.strftime('%Y-%m-%d %H:%M:%S'), vw_user_id))
         self.con.commit()
 
-    def delete_user(self, vw_user_id):
-        self.con.cursor().execute('DELETE FROM Users WHERE vw_user_id = ?', (vw_user_id,))
+    def delete_user(self, vw_user_id: str):
+        cursor = self.con.cursor()
+        cursor.execute('DELETE FROM Users WHERE vw_user_id = %s', (vw_user_id,))
         self.con.commit()
 
     def __del__(self):
